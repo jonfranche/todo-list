54b80d9aa60d7dd73843bb278284c5936c74d5f9
jonfranche <jonfranche@gmail.com>
2020-01-08 14:36:22 -0800

    added titles for addTodoForm inputs and changed the description input to a textarea element

--- src/Modules/addTodoForm.js
+++ src/Modules/addTodoForm.js
@@ -8,22 +8,27 @@ const addTodoForm = (div, list) => {
     todoForm.setAttribute('class', 'todoForm');
     div.appendChild(todoForm);
     const formElements = {
-        h2: document.createElement('h4'),
+        h4: document.createElement('h4'),
+        title: document.createElement('span'),
         titleInput: document.createElement('input'),
-        descInput: document.createElement('input'),
+        desc: document.createElement('span'),
+        descInput: document.createElement('textarea'),
+        dueDate: document.createElement('span'),
         dateInput: document.createElement('input'),
+        prio: document.createElement('span'),
         prioInput: document.createElement('input'),
         submit: document.createElement('button'),
         cancel: document.createElement('button'),
     }
 
-    formElements.h2.textContent = 'Add a New Todo';
-    formElements.titleInput.value = 'Title';
-    formElements.descInput.value = 'Description';
-    formElements.dateInput.value = 'Due Date (optional)';
-    formElements.prioInput.value = 'Priority: 1, 2, 3';
+    formElements.h4.textContent = 'Add a New Todo:';
+    formElements.title.textContent = 'Title:';
+    formElements.desc.textContent = 'Description:';
+    formElements.dueDate.textContent = 'Due Date (optional):';
+    formElements.prio.textContent = 'Priority: 1, 2, 3';
     formElements.submit.textContent = 'Submit';
     formElements.cancel.textContent = 'Cancel';
+    formElements.dateInput.setAttribute('type', 'date')
 
     
     for (let element in formElements) {
